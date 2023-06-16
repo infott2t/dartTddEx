bc0818a2166825d6e8f82924d927719988f05689
Hyeonil Choi <tt2t2am1118@naver.com>
2023-06-16 23:29:24 +0900

    잘 안되서 다시만듬. I remake, befor hard to me. Hope good devleoping. End today~. Thank you.

--- tddex1.dart
+++ tddex1.dart
@@ -1,163 +1,190 @@
 import 'dart:io';
 
-
-
-void main(List<String> arguments) {
-
-  CmdInit();
-}
-
-class CmdInit {
-
-  // Command String Formated Data
-  late List<List<String>> cmdList = [];
-  int cmdIndex = 0;
-
-
-  DateTime bootUpTime= DateTime.now();  // program boot up time
-
-  bool loginTF = false;   //  login true or false
-
-  CmdInit() {
-    bootUpTime = DateTime.now();
-    //Need http, save BootupTime.
-    inputData();
-
-    //처음에 해야할 일, 반복.
-    boot(cmdIndex);
-
-
-  }
-
-  //처음에 시작해야하는 일들.
-  void boot(int cmdIndex){
-
-    startCmdString();           // 메뉴 상위부분 출력
-    statusView();                // 사용자 상태 값 출력
-    cmdView(cmdList, cmdIndex); // cmdIndex값에 따라, cmdList[cmdIndex][3]. 메뉴를 출력.
-  }
-
-
-
-  void startCmdString() {
-    print("\n\n\n");
-    print("Starting....\n");
-    print("---- Hello, TDD. Test Driven Development. Fasting. ----\n");
-    print("Have a good day. today and time: ${DateTime.now()}\n");
-    print("This github, https://github.com/infott2t/dartTddEx");
-
-  }
-
-  void statusView() {
-    print("***** Status View *****\n");
-    print("Now bootUp time: $bootUpTime\n");
-    print("login time     : NONE\n");
-    print("login id       : N/A");
-    print("");
-    print("User Role      : --");
-    print("Now service    : --");
-    print("");
-  }
-
-
-
-
-  /*cmdList를 만들고,  '주요값'.                              0번째 Index값은 주요 명령어 스트링 이름, "Login"등으로 저장.
-                      'cmdList[index]의 이름'.               1번째 ''Index값은 실행해야할 메서드 또는 지정이름. cmdList[index]의 이름.
-                      'NumberQuestion || moveStringScreen'. 2번째는 숫자값을 물어보는 형태로 진행,'NumberQuestion'., 또는 결과페이지. cmdList의 마지막 인덱스 값이 cmdList 첫번째 인덱스 값과 일치하는 인덱스로 이동하기.
-                      '1.메뉴이름,2.메뉴이름,...'.             3번째는 숫자로 시작하는 메뉴들을 모아놓음. String변수. ','로 구분하여 저장.
-                      '리턴할 cmdList주요값'.                 4번째는 다시 돌아갈 메뉴. cmdList의 첫번째 Index값과 동일하게 만든다. 또는 실행해야할 메서드이름과 비슷하게 작성, 또는 ''.
-
-   */
-
-
-  void inputData() {
-    cmdList.add(['start',        'start',     'NumberQustion',    '1.Login,2.Logout,3.Service,4.Quit','']);
-    cmdList.add(['start,1.Login','login',     'MoveStringScreen', 'MoveScreenLoginTF','start,1.Login_resultTF']);
-    cmdList.add(['start,1.Login_resultTF','login_true','MoveStringScreen', 'Log activated.'                   ,'start']);
-    cmdList.add(['start,1.Login_resultTF','login_false','MoveStringScreen','Need registe[Y/N]'                ,'regist_chk']);
-
-
-  }
-
-  void cmdView(List<List<String>> cmdList, int cmdIndex) {
-    print("***** Command View *****\n");
-
-    // 명령어 메뉴값. split으로 각각 나눔. '1.메뉴이름, 2.메뉴이름,3.메뉴이름'
-    List<String> cmdStrSpt = cmdList[cmdIndex][3].split(',');
-
-    // 사용자에게 입력받는 숫자값. cmdList[cmdIndex][2]의 값이 'NumberQustion'일 경우 사용.
-    int input=0;
-
-
-
-    for (int i = 0; i < cmdStrSpt.length; i++) {
-      print("${cmdStrSpt[i]}");
-    }
-    print("");
-
-   //cmdList[cmdIndex][2]의 값이 'NumberQustion'일 경우, 숫자값을 입력받는 로직을 실행시킨다.
-    if(cmdList[cmdIndex][2] == 'NumberQustion') {
-      print("INPUT a number:");
-
-      try {
-        input = int.parse(stdin.readLineSync()!); // 숫자 입력 받기
-      }catch(e){
-        print(e);
-        print("Error. Back to First Menu.");
-        //input값이 예외값인 경우, 다시 cmdIndex값을 0으로 하여, 메뉴를 처음부터 작동시킨다.
-        boot(0);
-      }
-      input = input - 1; // 입력한 숫자와 메뉴 인덱스를 맞추기 위해 1을 뺌
-      print("***** Comment View *****\n");
-      print("Input Number is '${input +1}'. and cmdList[][3], '${cmdList[cmdIndex][3]}'. \n");
-      //cmdList[cmdIndex][3]의 값을 split한다.1
-      List<String> menuStrs = cmdList[cmdIndex][3].split(',');
-      print("So, menu value is '${menuStrs[input]}'. Next make code, //TODO.");
-      //메뉴값을 가져올 수 있으므로, 해당 일을 시켜주면 된다.
-      //TODO next this line~~. 메뉴값을 가져올수 있으므로, if문에 대입해서, cmdList를 진행시켜주면 됨니다.
-      //입력받은 input이 명령어 숫자를 넘어섰는지 확인.
-      try{
-
-      }catch(e){
-        print(e);
-        print("Error. Back to First Menu.");
-        //input값이 예외값인 경우, 다시 cmdIndex값을 0으로 하여, 메뉴를 처음부터 작동시킨다.
-        boot(0);
-      }
-
-      //cmdView(cmdList, cmdIndex);
-    } else if(cmdList[cmdIndex][2] == 'MoveStringScreen') {
-      print("${cmdList[cmdIndex][3]}");
-      cmdIndex = cmdList[cmdIndex][4] == '' ? 0 : cmdList[cmdIndex][4] == 'start' ? 0 : 1;
-      //cmdView(cmdList, cmdIndex);
-    } else {
-      print("Invalid menu option!");
-    }
-  }
-
-  void userInputStdin(){
-    print("Enter a number corresponding to the menu options:");
-    int input = int.parse(stdin.readLineSync()!); // 숫자 입력 받기
-  }
-
-  void executeNumberQuestion(String menu) {
-    print("Enter a number corresponding to the menu options:");
-    int input = int.parse(stdin.readLineSync()!); // 숫자 입력 받기
-
-    List<String> menuList = menu.split(','); // 메뉴 문자열을 ','로 분리하여 리스트로 저장
-    int index = input - 1; // 입력한 숫자와 메뉴 인덱스를 맞추기 위해 1을 뺌
-
-    if (index >= 0 && index < menuList.length) {
-      String selectedMenu = menuList[index].trim(); // 선택한 메뉴 가져오기 (앞뒤 공백 제거)
-      // 선택한 메뉴에 해당하는 동작 수행
-      // ...
-    } else {
-      print("Invalid menu option!");
-    }
-  }
-
-
+void main(){
+  MenuInit();
 }
 
 
+class MenuInit{
+
+  int menuIndex = 0;
+  List<List<String>> menuList = [];
+  bool loginTF = false;
+  String writeID = "";
+  String writePW = "";
+  String userRole="";
+
+  late DateTime loginTime;
+
+  MenuInit(){
+    print("--------------------");
+    print("Starting...");
+    print("Dart TDD~. \nDart programing. Test Driven Development(TDD). Example.\nLet's make this. After Extract model, service on Flutter, app.\nI'm making it. Not complete yet.");
+    print("Github, https://github.com/infott2t/dartTddEx");
+    print("Fasting.");
+    initMenuList();
+    boot(menuIndex);
+
+  }
+
+  void boot(int menuIndex) {
+    String resultMenu = "";
+    print("--------------------");
+    if (menuList[menuIndex][2] == 'NumberQuestion') {
+      //munuList[menuIndex][1]의 값을 split해서 print하기.
+      //메뉴값을 저장.
+      List<String> menuList0 = menuList[menuIndex][1].split(',');
+
+      for (int i = 0; i < menuList[menuIndex][1]
+          .split(',')
+          .length; i++) {
+        print("${menuList[menuIndex][1].split(',')[i]}");
+      }
+      //값 입력받기, stdin
+      print("Enter a number corresponding to the menu options:");
+      int input = int.parse(stdin.readLineSync()!); // 숫자 입력 받기
+
+
+      resultMenu = menuList[menuIndex][0] + "," + menuList0[input - 1];
+      //menuList에서 cmdList[menuIndex][0]의 값과 resultMenu값이 일치하는 부분을 찾기.
+      // print(resultMenu);
+      int i = 0;
+      for (i = 0; i < menuList.length; i++) {
+        if (menuList[i][0] == resultMenu) {
+          menuIndex = i;
+
+          break;
+        }
+      }
+      boot(i);
+    }
+    else if (menuList[menuIndex][2] == "chkBool") {
+      //메뉴 출력.
+      print(menuList[menuIndex][1]);
+
+      //로그인 체크.
+      if (menuList[menuIndex][0] == "start,1.Login") {
+        //TODO - http, 로그인 확인 하고 값 저장하기.
+        // loginTF = http....
+
+        if (loginTF) {
+          resultMenu = menuList[menuIndex][0] + ",T";
+          print("TRUE");
+        } else {
+          resultMenu = menuList[menuIndex][0] + ",F";
+          print("FALSE");
+        }
+      } else if (menuList[menuIndex][0] ==
+          "start,1.Login,F,Y,writingID,writingPassword") {
+        //TODO - http, 로그인 확인하고 값 저장하기.
+        // loginTF = http....
+        print("---ID, $writeID");
+        String resultMenu = "";
+        if (loginTF) {
+          resultMenu = menuList[menuIndex][0] + ",T";
+          print("TRUE");
+          boot(0);
+        } else {
+          resultMenu = menuList[menuIndex][0] + ",F";
+          print("FALSE");
+          boot(0);
+        }
+      }
+      //menuList에서 cmdList[menuIndex][0]의 값과 resultMenu값이 일치하는 부분을 찾기.
+      int i = 0;
+      for (i = 0; i < menuList.length; i++) {
+        if (menuList[i][0] == resultMenu) {
+          menuIndex = i;
+          //print(i);
+          break;
+        }
+      }
+
+      boot(i);
+    }
+    else if (menuList[menuIndex][2] == "YNQuestion") {
+      //메뉴출력.
+      print(menuList[menuIndex][1]);
+      //값 입력받기, stdin
+      print("Question, Y/N:");
+      String input = stdin.readLineSync()!; // Y/N 입력 받기.
+
+
+      if (input == "Y" || input == "y") {
+
+        resultMenu = menuList[menuIndex][0] + ",Y";
+      } else if (input == "N" || input == "n") {
+
+        resultMenu = menuList[menuIndex][0] + ",N";
+      } else {
+        boot(menuIndex);
+      }
+      //menuList에서 cmdList[menuIndex][0]의 값과 resultMenu값이 일치하는 부분을 찾기.
+      int i = 0;
+      for (i = 0; i < menuList.length; i++) {
+        if (menuList[i][0] == resultMenu) {
+          menuIndex = i;
+          //print(i);
+          break;
+        }
+      }
+      boot(i);
+    }
+    else if (menuList[menuIndex][2] == "StringQuestion") {
+      //메뉴출력.
+      print(menuList[menuIndex][1]);
+      //값 입력받기, stdin
+      print("String input:");
+      String input = stdin.readLineSync()!; // Y/N 입력 받기.
+      print(input);
+
+      if (input != null) {
+        if (menuList[menuIndex][0] == "start,1.Login,F,Y") {
+          writeID = input;
+          resultMenu = menuList[menuIndex][0] + ",writingID";
+        } else if (menuList[menuIndex][0] == "start,1.Login,F,Y,writingID") {
+          writePW = input;
+          resultMenu = menuList[menuIndex][0] + ",writingPassword";
+        }
+      }
+      //menuList에서 cmdList[menuIndex][0]의 값과 resultMenu값이 일치하는 부분을 찾기.
+      int i = 0;
+      for (i = 0; i < menuList.length; i++) {
+        if (menuList[i][0] == resultMenu) {
+          menuIndex = i;
+          //print(i);
+          break;
+        }
+      }
+      boot(i);
+    } else if(menuList[menuIndex][2] =="InitMenu"){
+      //메뉴출력.
+      print(menuList[menuIndex][1]);
+      boot(0);
+    }
+
+    else if (menuList[menuIndex][0] == "start,4.Status") {
+      print("Status");
+      if (loginTF) {
+        print("ID : $writeID");
+        print("User Role : $userRole");
+      } else {
+        print("No Login.");
+      }
+      boot(0);
+    }
+  }
+
+  void initMenuList() {
+    menuList.add(['start','1.Login,2.Logout,3.Service,4.Status,5.Regist,6.Exit','NumberQuestion']);
+    menuList.add(['start,1.Login','Check login...','chkBool']);
+    menuList.add(['start,1.Login,T','Activated.','start']);
+    menuList.add(['start,1.Login,F','Not Activated. \nLogin?[Y/N]','YNQuestion']);
+    menuList.add(['start,1.Login,F,Y','Login..., input ID. ','StringQuestion']);
+    menuList.add(['start,1.Login,F,Y,writingID','Login input Password.','StringQuestion']);
+    menuList.add(['start,1.Login,F,Y,writingID,writingPassword','Login input Password.','chkBool']);
+    menuList.add(['start,1.Login,F,Y,writingID,writingPassword,chkBool','Check login...','chkBool']);
+    menuList.add(['start,1.Login,F,N','No login. ','InitMenu']);
+    menuList.add(['start,4.Status','Status...','PrintStatus']);
+  }
+}
